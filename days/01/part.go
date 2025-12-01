ea8f5440a3ecf48fe63ea53375456c7fe317440f
Raphael Pour <info@raphaelpour.de>
2025-12-01 06:59:01 +0100

    do

--- days/01/part.go
+++ days/01/part.go
@@ -25,9 +25,6 @@ func part1(data []string) int {
 		}
 
 		dial = (dial + val) % 100
-
-		fmt.Printf("%s: %d\n", line, dial)
-
 		if dial == 0 {
 			zeroHits += 1
 		}
@@ -37,7 +34,37 @@ func part1(data []string) int {
 }
 
 func part2(data []string) int {
-	return 0
+	dial := 50
+	zeroHits := 0
+
+	fmt.Println("dial -> 50")
+
+	for i, line := range data {
+		if len(line) < 2 {
+			fmt.Printf("bad line %d: %s\n", i, line)
+			continue
+		}
+
+		val := strings.ToInt(line[1:])
+		if line[0] == 'L' {
+			val = 100 - val
+		}
+		rawVal := val % 100
+
+		if dial+rawVal <= 0 || dial+rawVal >= 100 {
+			zeroHits += 1
+		}
+
+		dial = (dial + val) % 100
+
+		fmt.Printf("%s: %d\n", line, dial)
+
+		/*if dial == 0 {
+			zeroHits += 1
+		}*/
+	}
+
+	return zeroHits
 }
 
 func main() {
@@ -47,6 +74,7 @@ func main() {
 	fmt.Println("bad: 31")
 	fmt.Println(part1(data))
 
-	// fmt.Println("== [ PART 2 ] ==")
-	// fmt.Println(part2(data))
+	fmt.Println("== [ PART 2 ] ==")
+	fmt.Println("bad: 1889,2296,2347,2357,3492")
+	fmt.Println(part2(data))
 }
