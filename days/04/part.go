d749cccd2c39b349a65656af5d5a2d5efa45186e
Raphael Pour <info@raphaelpour.de>
2025-12-04 13:57:08 +0100

    04

--- days/04/part.go
+++ days/04/part.go
@@ -23,23 +23,57 @@ func NewGrid(input []string)Grid{
 	return Grid{fields:fields}
 }
 
+func (g Grid) Within(x,y int) bool{
+	  // neighbor can't have a negative position
+	  if x <0{
+			return false
+	  }
+	  if y <0{
+			return false
+	  }
+
+	  // neighbor can't be outside the range
+	  if x >= len(g.fields) || y >= len(g.fields){
+			return false
+	  }
+	return true
+}
+
+func (g Grid) String() string{
+	output := ""
+	for y := range g.fields{
+		for x := range g.fields[y]{
+			if g.fields[y][x]{
+				output += "@"
+			} else{
+				output += "."
+			}
+		}
+		output += "\n"
+	}
+
+	return output
+}
+
 func (g Grid) Neighbours(x,y int) int{
 	neighbours := 0
     for xOff := -1; xOff <=1;xOff++{
 	    for yOff := -1; yOff <=1;yOff++{
-		
-		if y + yOff <0 || y+yOff >= len(g.fields){
+			// Don't count itself
+			if xOff == 0 && yOff == 0{
+				continue
+			}
+
+		  // possible neighbor position
+		  ox,oy := x+xOff,y+yOff
+
+		  if !g.Within(ox,oy){
 			continue
-		}
-		if x + xOff <0 || x+xOff >= len(g.fields[y]){
-			continue
-		}
-		if xOff == 0 && yOff == 0{
-			continue
-		}
-		if g.fields[y+yOff][x+xOff]{
-			neighbours +=1
-		}
+		  }
+
+			if g.fields[oy][ox]{
+				neighbours +=1
+			}
 	    }
     }
     return neighbours
@@ -50,8 +84,7 @@ func part1(data []string) int {
 	forkliftable := 0
 	for y := range grid.fields{
 	  for x := range grid.fields[y]{
-	    if grid.Neighbours(x,y) < 4{
-		    fmt.Println(x,y,grid.Neighbours(x,y))
+	    if grid.fields[y][x] && grid.Neighbours(x,y) < 4{
 		forkliftable +=1
 	    }
 	  }
@@ -60,18 +93,32 @@ func part1(data []string) int {
 }
 
 func part2(data []string) int {
-    return 0
+	grid := NewGrid(data)
+	count := 0
+	for {
+		forkliftable := 0
+		for y := range grid.fields{
+		  for x := range grid.fields[y]{
+			if grid.fields[y][x] && grid.Neighbours(x,y) < 4{
+				grid.fields[y][x] = false
+				forkliftable +=1
+				count +=1
+			}
+		  }
+		}
+		if forkliftable == 0{
+			break
+		}
+	}
+    return count
 }
 
 func main() {
-	// data := input.LoadString("input")
-	// data := input.LoadDefaultInt()
-	// data := input.LoadInt("input")
 	data := input.LoadDefaultString()
     
     fmt.Println("== [ PART 1 ] ==")
     fmt.Println(part1(data))
 
-    // fmt.Println("== [ PART 2 ] ==")
-    // fmt.Println(part2(data))
+    fmt.Println("== [ PART 2 ] ==")
+    fmt.Println(part2(data))
 }
