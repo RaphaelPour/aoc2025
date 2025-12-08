e2d15c4603e4a71c637e260abaa071561799d103
Raphael Pour <info@raphaelpour.de>
2025-12-08 07:52:51 +0100

    04: add + format

--- days/04/part.go
+++ days/04/part.go
@@ -6,46 +6,46 @@ import (
 	"github.com/RaphaelPour/stellar/input"
 )
 
-type Grid struct{
+type Grid struct {
 	fields [][]bool
 }
 
-func NewGrid(input []string)Grid{
+func NewGrid(input []string) Grid {
 	fields := make([][]bool, len(input))
 
-	for y, row := range input{
-	  fields[y] = make([]bool, len(row))
-	  for x, field := range row{
-	    fields[y][x] = field == '@'
-	  }
+	for y, row := range input {
+		fields[y] = make([]bool, len(row))
+		for x, field := range row {
+			fields[y][x] = field == '@'
+		}
 	}
 
-	return Grid{fields:fields}
+	return Grid{fields: fields}
 }
 
-func (g Grid) Within(x,y int) bool{
-	  // neighbor can't have a negative position
-	  if x <0{
-			return false
-	  }
-	  if y <0{
-			return false
-	  }
+func (g Grid) Within(x, y int) bool {
+	// neighbor can't have a negative position
+	if x < 0 {
+		return false
+	}
+	if y < 0 {
+		return false
+	}
 
-	  // neighbor can't be outside the range
-	  if x >= len(g.fields) || y >= len(g.fields){
-			return false
-	  }
+	// neighbor can't be outside the range
+	if x >= len(g.fields) || y >= len(g.fields) {
+		return false
+	}
 	return true
 }
 
-func (g Grid) String() string{
+func (g Grid) String() string {
 	output := ""
-	for y := range g.fields{
-		for x := range g.fields[y]{
-			if g.fields[y][x]{
+	for y := range g.fields {
+		for x := range g.fields[y] {
+			if g.fields[y][x] {
 				output += "@"
-			} else{
+			} else {
 				output += "."
 			}
 		}
@@ -55,41 +55,41 @@ func (g Grid) String() string{
 	return output
 }
 
-func (g Grid) Neighbours(x,y int) int{
+func (g Grid) Neighbours(x, y int) int {
 	neighbours := 0
-    for xOff := -1; xOff <=1;xOff++{
-	    for yOff := -1; yOff <=1;yOff++{
+	for xOff := -1; xOff <= 1; xOff++ {
+		for yOff := -1; yOff <= 1; yOff++ {
 			// Don't count itself
-			if xOff == 0 && yOff == 0{
+			if xOff == 0 && yOff == 0 {
 				continue
 			}
 
-		  // possible neighbor position
-		  ox,oy := x+xOff,y+yOff
+			// possible neighbor position
+			ox, oy := x+xOff, y+yOff
 
-		  if !g.Within(ox,oy){
-			continue
-		  }
-
-			if g.fields[oy][ox]{
-				neighbours +=1
+			if !g.Within(ox, oy) {
+				continue
 			}
-	    }
-    }
-    return neighbours
+
+			if g.fields[oy][ox] {
+				neighbours += 1
+			}
+		}
+	}
+	return neighbours
 }
 
 func part1(data []string) int {
 	grid := NewGrid(data)
 	forkliftable := 0
-	for y := range grid.fields{
-	  for x := range grid.fields[y]{
-	    if grid.fields[y][x] && grid.Neighbours(x,y) < 4{
-		forkliftable +=1
-	    }
-	  }
+	for y := range grid.fields {
+		for x := range grid.fields[y] {
+			if grid.fields[y][x] && grid.Neighbours(x, y) < 4 {
+				forkliftable += 1
+			}
+		}
 	}
-    return forkliftable
+	return forkliftable
 }
 
 func part2(data []string) int {
@@ -97,28 +97,28 @@ func part2(data []string) int {
 	count := 0
 	for {
 		forkliftable := 0
-		for y := range grid.fields{
-		  for x := range grid.fields[y]{
-			if grid.fields[y][x] && grid.Neighbours(x,y) < 4{
-				grid.fields[y][x] = false
-				forkliftable +=1
-				count +=1
+		for y := range grid.fields {
+			for x := range grid.fields[y] {
+				if grid.fields[y][x] && grid.Neighbours(x, y) < 4 {
+					grid.fields[y][x] = false
+					forkliftable += 1
+					count += 1
+				}
 			}
-		  }
 		}
-		if forkliftable == 0{
+		if forkliftable == 0 {
 			break
 		}
 	}
-    return count
+	return count
 }
 
 func main() {
 	data := input.LoadDefaultString()
-    
-    fmt.Println("== [ PART 1 ] ==")
-    fmt.Println(part1(data))
 
-    fmt.Println("== [ PART 2 ] ==")
-    fmt.Println(part2(data))
+	fmt.Println("== [ PART 1 ] ==")
+	fmt.Println(part1(data))
+
+	fmt.Println("== [ PART 2 ] ==")
+	fmt.Println(part2(data))
 }
--- days/04/part_test.go
+++ days/04/part_test.go
@@ -1,13 +1,165 @@
 package main
 
 import (
-    "testing"
-
-        "github.com/stretchr/testify/require"
+	"reflect"
+	"testing"
 
 	"github.com/RaphaelPour/stellar/input"
+	"github.com/stretchr/testify/require"
 )
 
 func TestExample(t *testing.T) {
-    require.Equal(t, 13, part1(input.LoadString("input_test")))
+	require.Equal(t, 13, part1(input.LoadString("input_test")))
+}
+
+func TestNewGrid(t *testing.T) {
+	type args struct {
+		input []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Grid
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewGrid(tt.args.input); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewGrid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGrid_Within(t *testing.T) {
+	type fields struct {
+		fields [][]bool
+	}
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := Grid{
+				fields: tt.fields.fields,
+			}
+			if got := g.Within(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("Grid.Within() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGrid_String(t *testing.T) {
+	type fields struct {
+		fields [][]bool
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := Grid{
+				fields: tt.fields.fields,
+			}
+			if got := g.String(); got != tt.want {
+				t.Errorf("Grid.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGrid_Neighbours(t *testing.T) {
+	type fields struct {
+		fields [][]bool
+	}
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := Grid{
+				fields: tt.fields.fields,
+			}
+			if got := g.Neighbours(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("Grid.Neighbours() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_part1(t *testing.T) {
+	type args struct {
+		data []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part1(tt.args.data); got != tt.want {
+				t.Errorf("part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_part2(t *testing.T) {
+	type args struct {
+		data []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part2(tt.args.data); got != tt.want {
+				t.Errorf("part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
 }
